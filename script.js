22b4e3ba4d4b6f421599445810ae85b8a48f2b94
Sh1zzuk1 <157670400+Sh1zzuk1@users.noreply.github.com>
2024-05-17 23:38:09 +0300

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function showCatalog(catalogId) {
-    document.getElementById('catalog1').classList.remove('active');
-    document.getElementById('catalog2').classList.remove('active');
-
+    var catalogs = document.querySelectorAll('.catalog');
+    catalogs.forEach(function(catalog) {
+        catalog.classList.remove('active');
+    });
     document.getElementById(catalogId).classList.add('active');
 }
-
-// Получаем цвета из Telegram (предполагается, что они будут переданы при инициализации веб-страницы)
-document.documentElement.style.setProperty('--telegram-background-color', '#f0f0f0'); // Замените на реальный цвет из Telegram
-document.documentElement.style.setProperty('--telegram-button-color', '#6200ea'); // Замените на реальный цвет из Telegram
-document.documentElement.style.setProperty('--telegram-button-hover-color', '#3700b3'); // Замените на реальный цвет при наведении
